70597dbc74103f6c9b743df5b74a04e725f649b6
Jaeyong <study@snu.ac.kr>
2021-12-31 19:44:26 +0900

    fixed pdf path

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 // If absolute URL from the remote server is provided, configure the CORS
 // header on that server.
-var url = 'https://raw.githubusercontent.com/teachsavvy/koredusurvey/main/test/reading.pdf';
+var url = 'https://raw.githubusercontent.com/teachsavvy/koredusurvey/main/reading.pdf';
 
 // Loaded via <script> tag, create shortcut to access PDF.js exports.
 var pdfjsLib = window['pdfjs-dist/build/pdf'];
